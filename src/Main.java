48c92633be764d06d75ed82d02f1ca961ec41671
dewey <dcancer17@gmail.com>
2025-01-15 18:53:29 +0900

    finish downloader 250115

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ import java.net.HttpURLConnection;
 public class Main {
     public static void main(String[] args) throws IOException {
         // 목록 화면에서 Url 입력
-        String url = "https://newtoki.biz/book/19874";
+        String url = "";
         // 다운로드 받을 장소는 FileDownloader.Downloader.downLoadPath
 
         Downloader downloader = new Downloader(url);
