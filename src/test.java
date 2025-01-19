b57ccb60c8898efdd396401bf81e718f4bb049c7
dewey <dcancer17@gmail.com>
2025-01-19 19:34:33 +0900

    test

--- src/test.java
+++ src/test.java
@@ -1,2 +1,15 @@
+import FIleDownloader.ConnectListUrl;
+import FIleDownloader.SaveText;
+
+import java.io.IOException;
+import java.util.List;
+
 public class test {
+    public static void main(String[] args) throws IOException {
+        ConnectListUrl connectListUrl = new ConnectListUrl("");
+        List<String> urlResult = connectListUrl.openConnect();
+        SaveText saveText = new SaveText("sakamotoDays", urlResult);
+        saveText.save();
+
+    }
 }
