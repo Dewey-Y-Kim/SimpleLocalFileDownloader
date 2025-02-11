aead40b96f11332962128001891789348778ee98
dewey <dcancer17@gmail.com>
2025-02-11 15:10:22 +0900

    Image Downloader

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,5 +1,6 @@
 package test.java;
 
+import main.java.file_downloader.Downloader;
 import main.java.file_downloader.connector.ConnectListUrl;
 import main.java.file_downloader.connector.Connector;
 
@@ -21,14 +22,15 @@ import java.util.regex.Pattern;
 public class test{
 
     public static void main(String[] args) throws URISyntaxException, IOException, NoSuchAlgorithmException, KeyManagementException {
-        // 자바에서 사용시 vpn 켜고 사용할 것
-        URI uri = new URI("");
-        URL url = uri.toURL();
-        HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
-        connection.setConnectTimeout(3000);
-        
-        connection.setRequestMethod("GET");
-        System.out.println(connection.getResponseCode());
+        // 자바에서 사용시 mtu 513 정상가동
+        new Downloader("https://11toon144.com/bbs/board.php?bo_table=toons&stx=%EC%97%B4%ED%98%88%EA%B0%95%ED%98%B8&is=1").makeImageList();
+//        URI uri = new URI("https://11toon144.com/bbs/board.php?bo_table=toons&stx=%EC%97%B4%ED%98%88%EA%B0%95%ED%98%B8&is=1");
+//        URL url = uri.toURL();
+//        HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+//        connection.setConnectTimeout(3000);
+//
+//        connection.setRequestMethod("GET");
+//        System.out.println(connection.getResponseCode());
 //
 //        InputStream inputStream = connection.getInputStream();
 //        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader( inputStream));
